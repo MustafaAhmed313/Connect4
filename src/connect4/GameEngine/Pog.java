c0ac0befb6cf0513117b26d136ae520c9142cde5
Mustafa Ahmed <mostafa3132004@gmail.com>
2023-12-10 20:06:18 +0200

    Modify .gitignore file
    - ignore .xml files
    - ignore .mf files
    - ignore nbproject

--- src/connect4/GameEngine/Pog.java
+++ src/connect4/GameEngine/Pog.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package GameEngine;
+package connect4.GameEngine;
 
 /**
  *
  * @author mosta
  */
-public class Tuple {
+public class Pog {
     
 }
