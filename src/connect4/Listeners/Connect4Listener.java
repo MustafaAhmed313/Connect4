18cb6f330440d0053605f13c61b67ca6af70db02
Mustafa Ahmed <mostafa3132004@gmail.com>
2023-12-10 20:26:04 +0200

    edit Listeners

--- src/connect4/Listeners/Connect4Listener.java
+++ src/connect4/Listeners/Connect4Listener.java
@@ -2,13 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Listeners;
+package connect4.Listeners;
 
 /**
  *
  * @author mosta
  */
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
