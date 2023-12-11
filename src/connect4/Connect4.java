eee2144c507612f5b8a0a7ad94134b9f0291feeb
Mustafa Ahmed <mostafa3132004@gmail.com>
2023-12-11 07:41:08 +0200

    run-the-env

--- src/connect4/Connect4.java
+++ src/connect4/Connect4.java
@@ -15,6 +15,7 @@ public class Connect4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hi, I'm poared!");
     }
     
 }
