c705e06b57ee61dcab3c33217c402c5ef5784b93
Mustafa Ahmed <mostafa3132004@gmail.com>
2023-12-11 20:23:46 +0200

    Add The New Engine

--- src/connect4/GameEngine/Engine.java
+++ src/connect4/GameEngine/Engine.java
@@ -8,7 +8,101 @@ import java.util.Arrays;
 
 /**
  *
- * @author mosta
+ * @author youssef Amir
  */
 public class Engine {
+
+        private static final int ROWS = 6;
+        private static final int COLUMNS = 7;
+        private char[][] board;
+        private char currentPlayer;
+
+        public Engine() {
+            board = new char[ROWS][COLUMNS];
+            for (char[] row : board) {
+                Arrays.fill(row, ' ');
+            }
+            currentPlayer = 'X';
+        }
+
+        public void printBoard() {
+            for (char[] row : board) {
+                System.out.println(Arrays.toString(row));
+            }
+        }
+
+        public boolean dropToken(int column) {
+            for (int row = ROWS - 1; row >= 0; row--) {
+                if (board[row][column] == ' ') {
+                    board[row][column] = currentPlayer;
+                    return true;
+                }
+            }
+            return false; // Column is full
+        }
+
+        public boolean checkWin() {
+            return checkHorizontal() || checkVertical() || checkDiagonal();
+        }
+
+        private boolean checkHorizontal() {
+            for (int row = 0; row < ROWS; row++) {
+                for (int col = 0; col < COLUMNS - 3; col++) {
+                    if (board[row][col] != ' '
+                            && board[row][col] == board[row][col + 1]
+                            && board[row][col] == board[row][col + 2]
+                            && board[row][col] == board[row][col + 3]) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private boolean checkVertical() {
+            for (int row = 0; row < ROWS - 3; row++) {
+                for (int col = 0; col < COLUMNS; col++) {
+                    if (board[row][col] != ' '
+                            && board[row][col] == board[row + 1][col]
+                            && board[row][col] == board[row + 2][col]
+                            && board[row][col] == board[row + 3][col]) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private boolean checkDiagonal() {
+            for (int row = 0; row < ROWS - 3; row++) {
+                for (int col = 0; col < COLUMNS - 3; col++) {
+                    if (board[row][col] != ' '
+                            && board[row][col] == board[row + 1][col + 1]
+                            && board[row][col] == board[row + 2][col + 2]
+                            && board[row][col] == board[row + 3][col + 3]) {
+                        return true;
+                    }
+                }
+            }
+
+            for (int row = 3; row < ROWS; row++) {
+                for (int col = 0; col < COLUMNS - 3; col++) {
+                    if (board[row][col] != ' '
+                            && board[row][col] == board[row - 1][col + 1]
+                            && board[row][col] == board[row - 2][col + 2]
+                            && board[row][col] == board[row - 3][col + 3]) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public char getCurrentPlayer() {
+            return currentPlayer;
+        }
+
+        public void switchPlayer() {
+            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+        }
 }
